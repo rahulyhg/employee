887f25c390bd6c017d8c5031a0c727a6146f5178
Tu TV <tutv95@gmail.com>
2016-04-08 22:49:45 +0700

    Add migration

--- database/migrations/2016_04_08_120335_create_employees_table.php
+++ database/migrations/2016_04_08_120335_create_employees_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->increments('id');
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('activated');
-            $table->string('activated_code');
-            $table->rememberToken();
+            $table->string('photo');
+            $table->string('phone');
+            $table->string('job');
+            $table->integer('department_id');
+
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('employees');
     }
 }
--- database/migrations/2016_04_08_120400_create_departments_table.php
+++ database/migrations/2016_04_08_120400_create_departments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateDepartmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->increments('id');
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('activated');
-            $table->string('activated_code');
-            $table->rememberToken();
+            $table->integer('manager_id');
+            $table->string('phone');
+
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('departments');
     }
 }
