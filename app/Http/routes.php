f8fe93c49413535b8cbe614c050b212df0c840c5
Tu TV <tutv95@gmail.com>
2016-04-23 16:59:58 +0700

    Updated home page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,70 +11,69 @@
 |
 */
 
-Route::get('/', function () {
-	return view('welcome');
-});
+Route::get( '/', [
+	'as' => 'home',
+	'uses' => 'HomeController@index'
+] );
 
 Route::auth();
-
-Route::get('/home', 'HomeController@index');
 
 /**
  * Department
  */
-Route::group(['prefix' => 'department'], function () {
-	Route::get('/', [
-		'as' => 'department.index',
+Route::group( [ 'prefix' => 'department' ], function () {
+	Route::get( '/', [
+		'as'   => 'department.index',
 		'uses' => 'DepartmentController@index'
-	]);
+	] );
 
-	Route::get('/{id}', [
-		'as' => 'department.show',
+	Route::get( '/{id}', [
+		'as'   => 'department.show',
 		'uses' => 'DepartmentController@show'
-	]);
-});
+	] );
+} );
 
 /**
  * Employee
  */
-Route::group(['prefix' => 'employee'], function () {
-	Route::get('/', [
-		'as' => 'employee.index',
+Route::group( [ 'prefix' => 'employee' ], function () {
+	Route::get( '/', [
+		'as'   => 'employee.index',
 		'uses' => 'EmployeeController@index'
-	]);
+	] );
 
-	Route::get('add', [
-		'as' => 'employee.add',
+	Route::get( 'add', [
+		'as'   => 'employee.add',
 		'uses' => 'EmployeeController@addShow',
-	]);
+	] );
 
-	Route::get('edit/{id}', [
-		'as' => 'employee.edit',
+	Route::get( 'edit/{id}', [
+		'as'   => 'employee.edit',
 		'uses' => 'EmployeeController@editShow',
-	])->where('id', '[0-9]+');
+	] )->where( 'id', '[0-9]+' );
 
 	/**
 	 * Show
 	 */
-	Route::get('{id}', [
-		'as' => 'employee.show',
+	Route::get( '{id}', [
+		'as'   => 'employee.show',
 		'uses' => 'EmployeeController@show'
-	])->where('id', '[0-9]+');
-});
+	] )->where( 'id', '[0-9]+' );
+} );
 
 /**
  * Ajax
  */
-Route::group(['prefix' => 'ajax'], function () {
-	Route::group(['prefix' => 'employee'], function () {
-		Route::post('add', [
-			'as' => 'ajax.employee.add',
+Route::group( [ 'prefix' => 'ajax' ], function () {
+	Route::group( [ 'prefix' => 'employee' ], function () {
+		Route::post( 'add', [
+			'as'   => 'ajax.employee.add',
 			'uses' => 'AjaxController@addEmployee',
-		]);
+		] );
 
-		Route::post('edit', [
-			'as' => 'ajax.employee.edit',
+		Route::post( 'edit', [
+			'as'   => 'ajax.employee.edit',
 			'uses' => 'AjaxController@editEmployee',
-		]);
-	});
-});
+		] );
+	} );
+} );
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,107 +42,29 @@
     <div class="departments">
         <div class="container">
             <div class="f-heading text-center">
-                <h2 class="primary">Department</h2>
+                <h2 class="primary">Departments</h2>
                 <div class="secondary">Employees lorem ipsum ext sane uet.</div>
                 <span class="line"></span>
             </div>
 
             <div class="row">
-                <div class="col-md-4">
-                    <div class="department">
-                        <div class="img">
-                            <a href="#">
-                                <img src="{{ asset('assets/images/department_test.jpg') }}" alt="test" class="img-responsive">
-                            </a>
-                        </div>
+                @foreach($departments as $index => $department)
+                    <div class="col-md-4">
+                        <div class="department">
+                            <div class="img">
+                                <a href="{{ route('department.show', $department->id) }}" title="{{ $department->name }}">
+                                    <img src="{{ asset('assets/images/department_test.jpg') }}" alt="test" class="img-responsive">
+                                </a>
+                            </div>
 
-                        <div class="details">
-                            <h3 class="name"><a href="#">Department 1</a></h3>
-                            <div class="manager">Manager: <strong>David Beckham</strong></div>
-                            <div class="employees">Employees: <strong>100</strong></div>
+                            <div class="details">
+                                <h3 class="name"><a href="{{ route('department.show', $department->id) }}">{{ $department->name }}</a></h3>
+                                <div class="manager">Manager: <strong>{{ $department->manager->name }}</strong></div>
+                                <div class="employees">Employees: <strong>{{ count($department->employees) }}</strong></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="department">
-                        <div class="img">
-                            <a href="#">
-                                <img src="{{ asset('assets/images/department_test.jpg') }}" alt="test" class="img-responsive">
-                            </a>
-                        </div>
-
-                        <div class="details">
-                            <h3 class="name"><a href="#">Department 1</a></h3>
-                            <div class="manager">Manager: <strong>David Beckham</strong></div>
-                            <div class="employees">Employees: <strong>100</strong></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="department">
-                        <div class="img">
-                            <a href="#">
-                                <img src="{{ asset('assets/images/department_test.jpg') }}" alt="test" class="img-responsive">
-                            </a>
-                        </div>
-
-                        <div class="details">
-                            <h3 class="name"><a href="#">Department 1</a></h3>
-                            <div class="manager">Manager: <strong>David Beckham</strong></div>
-                            <div class="employees">Employees: <strong>100</strong></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="department">
-                        <div class="img">
-                            <a href="#">
-                                <img src="{{ asset('assets/images/department_test.jpg') }}" alt="test" class="img-responsive">
-                            </a>
-                        </div>
-
-                        <div class="details">
-                            <h3 class="name"><a href="#">Department 1</a></h3>
-                            <div class="manager">Manager: <strong>David Beckham</strong></div>
-                            <div class="employees">Employees: <strong>100</strong></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="department">
-                        <div class="img">
-                            <a href="#">
-                                <img src="{{ asset('assets/images/department_test.jpg') }}" alt="test" class="img-responsive">
-                            </a>
-                        </div>
-
-                        <div class="details">
-                            <h3 class="name"><a href="#">Department 1</a></h3>
-                            <div class="manager">Manager: <strong>David Beckham</strong></div>
-                            <div class="employees">Employees: <strong>100</strong></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="department">
-                        <div class="img">
-                            <a href="#">
-                                <img src="{{ asset('assets/images/department_test.jpg') }}" alt="test" class="img-responsive">
-                            </a>
-                        </div>
-
-                        <div class="details">
-                            <h3 class="name"><a href="#">Department 1</a></h3>
-                            <div class="manager">Manager: <strong>David Beckham</strong></div>
-                            <div class="employees">Employees: <strong>100</strong></div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
