6a92d741b6114fa2e5e060943123fa57feb0fb42
Tu TV <tutv95@gmail.com>
2016-04-25 23:39:19 +0700

    Updated change profile

--- resources/views/users/profile-form.blade.php
+++ resources/views/users/profile-form.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ $form['url'] }}" method="{{ $form['method'] }}" class="form-profile">
     {{ csrf_field() }}
 
-    <div class="form-group{!! ($errors->has('password')) ? ' has-errors' : '' !!}" data-input="name">
+    <div class="form-group{!! ($errors->has('name')) ? ' has-errors' : '' !!}" data-input="name">
         <label for="name">Name</label>
         <input name="name" id="name" type="text" class="form-control" value="{{ $form['defaults']['name'] }}" placeholder="Name">
         <div class="errors help-block">{{ ($errors->has('name') ? $errors->first('name') : '') }}</div>
